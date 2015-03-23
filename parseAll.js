6e62ffde5ce0479fdb0a8f6232fea5a112e9625a
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-23 07:05:59 -0300

    pure esprima

--- parseAll.js
+++ parseAll.js
@@ -3,17 +3,27 @@ require('babel/polyfill');
 
 var path = require('path');
 var Parser = require('azk-parser').Parser;
-var parser = new Parser();
 var Generator = require('azk-parser').Generator;
 var fileUtils = require('azk-parser').fileUtils;
 var bb = require('bluebird');
 var glob = fileUtils.glob;
+var esprima = require('esprima-fb');
+
+var parser = new Parser({
+  raw: false,
+  tokens: false,
+  range: false,
+  comment: false,
+  loc: false
+});
 
 glob('./from-code/**/*.js', {}, function(err, files) {
   files.forEach(function(file) {
     fileUtils.read(file).then(function(content) {
       // parse code to AST
-      var ast = parser.parse(content).syntax;
+      // var ast = parser.parse(content).syntax;
+      var ast = esprima.parse(content,
+        {raw: false, tokens: false, range: false, comment: false, loc: false });
 
       // get destination path
       var parsed_path = path.parse(file);
--- to-ast/systems.js
+++ to-ast/systems.js
@@ -1,276 +1,52 @@
 {
-  "program": {
-    "type": "Program",
-    "body": [
-      {
-        "type": "ExpressionStatement",
-        "expression": {
-          "type": "CallExpression",
-          "callee": {
-            "type": "Identifier",
-            "name": "systems",
-            "range": [
-              11,
-              18
-            ],
-            "loc": {
-              "start": {
-                "line": 2,
-                "column": 0
-              },
-              "end": {
-                "line": 2,
-                "column": 7
-              },
-              "lines": {},
-              "indent": 0
-            }
-          },
-          "arguments": [
-            {
-              "type": "ObjectExpression",
-              "properties": [
-                {
-                  "type": "Property",
-                  "key": {
-                    "type": "Identifier",
-                    "name": "system1",
-                    "range": [
-                      25,
-                      32
-                    ],
-                    "loc": {
-                      "start": {
-                        "line": 3,
-                        "column": 4
-                      },
-                      "end": {
-                        "line": 3,
-                        "column": 11
-                      },
-                      "lines": {},
-                      "indent": 4
-                    }
-                  },
-                  "value": {
-                    "type": "ObjectExpression",
-                    "properties": [],
-                    "range": [
-                      34,
-                      36
-                    ],
-                    "loc": {
-                      "start": {
-                        "line": 3,
-                        "column": 13
-                      },
-                      "end": {
-                        "line": 3,
-                        "column": 15
-                      },
-                      "lines": {},
-                      "indent": 4
-                    }
-                  },
-                  "kind": "init",
-                  "method": false,
-                  "shorthand": false,
-                  "computed": false,
-                  "range": [
-                    25,
-                    36
-                  ],
-                  "loc": {
-                    "start": {
-                      "line": 3,
-                      "column": 4
-                    },
-                    "end": {
-                      "line": 3,
-                      "column": 15
-                    },
-                    "lines": {},
-                    "indent": 4
-                  }
-                },
-                {
-                  "type": "Property",
-                  "key": {
-                    "type": "Identifier",
-                    "name": "system1",
-                    "range": [
-                      42,
-                      49
-                    ],
-                    "loc": {
-                      "start": {
-                        "line": 4,
-                        "column": 4
-                      },
-                      "end": {
-                        "line": 4,
-                        "column": 11
-                      },
-                      "lines": {},
-                      "indent": 4
-                    }
-                  },
-                  "value": {
-                    "type": "ObjectExpression",
-                    "properties": [],
-                    "range": [
-                      51,
-                      53
-                    ],
-                    "loc": {
-                      "start": {
-                        "line": 4,
-                        "column": 13
-                      },
-                      "end": {
-                        "line": 4,
-                        "column": 15
-                      },
-                      "lines": {},
-                      "indent": 4
-                    }
-                  },
-                  "kind": "init",
-                  "method": false,
-                  "shorthand": false,
-                  "computed": false,
-                  "range": [
-                    42,
-                    53
-                  ],
-                  "loc": {
-                    "start": {
-                      "line": 4,
-                      "column": 4
-                    },
-                    "end": {
-                      "line": 4,
-                      "column": 15
-                    },
-                    "lines": {},
-                    "indent": 4
-                  }
-                }
-              ],
-              "range": [
-                19,
-                55
-              ],
-              "loc": {
-                "start": {
-                  "line": 2,
-                  "column": 8
-                },
-                "end": {
-                  "line": 5,
-                  "column": 1
-                },
-                "lines": {},
-                "indent": 0
-              }
-            }
-          ],
-          "range": [
-            11,
-            56
-          ],
-          "loc": {
-            "start": {
-              "line": 2,
-              "column": 0
-            },
-            "end": {
-              "line": 5,
-              "column": 2
-            },
-            "lines": {},
-            "indent": 0
-          }
+  "type": "Program",
+  "body": [
+    {
+      "type": "ExpressionStatement",
+      "expression": {
+        "type": "CallExpression",
+        "callee": {
+          "type": "Identifier",
+          "name": "systems"
         },
-        "range": [
-          11,
-          57
-        ],
-        "loc": {
-          "start": {
-            "line": 2,
-            "column": 0
-          },
-          "end": {
-            "line": 5,
-            "column": 3
-          },
-          "lines": {},
-          "indent": 0
-        },
-        "comments": [
+        "arguments": [
           {
-            "type": "Line",
-            "value": " comment",
-            "range": [
-              0,
-              10
-            ],
-            "loc": {
-              "start": {
-                "line": 1,
-                "column": 0
+            "type": "ObjectExpression",
+            "properties": [
+              {
+                "type": "Property",
+                "key": {
+                  "type": "Identifier",
+                  "name": "system1"
+                },
+                "value": {
+                  "type": "ObjectExpression",
+                  "properties": []
+                },
+                "kind": "init",
+                "method": false,
+                "shorthand": false,
+                "computed": false
               },
-              "end": {
-                "line": 1,
-                "column": 10
-              },
-              "lines": {},
-              "indent": 0
-            },
-            "leading": true,
-            "trailing": false
+              {
+                "type": "Property",
+                "key": {
+                  "type": "Identifier",
+                  "name": "system2"
+                },
+                "value": {
+                  "type": "ObjectExpression",
+                  "properties": []
+                },
+                "kind": "init",
+                "method": false,
+                "shorthand": false,
+                "computed": false
+              }
+            ]
           }
         ]
       }
-    ],
-    "range": [
-      11,
-      57
-    ],
-    "loc": {
-      "lines": {},
-      "start": {
-        "line": 1,
-        "column": 0
-      },
-      "end": {
-        "line": 5,
-        "column": 3
-      },
-      "indent": 0
-    },
-    "errors": [
-      {
-        "index": 53,
-        "lineNumber": 4,
-        "column": 16,
-        "description": "Duplicate data property in object literal not allowed in strict mode"
-      }
-    ]
-  },
-  "loc": {
-    "lines": {},
-    "indent": 0,
-    "start": {
-      "line": 1,
-      "column": 0
-    },
-    "end": {
-      "line": 5,
-      "column": 3
     }
-  },
-  "type": "File",
-  "comments": null
+  ]
 }
