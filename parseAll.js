dea8bb7b890e0c6e39164943c91251e6aa91007b
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-23 07:13:56 -0300

    will parse only with esprima

--- parseAll.js
+++ parseAll.js
@@ -2,28 +2,33 @@
 require('babel/polyfill');
 
 var path = require('path');
-var Parser = require('azk-parser').Parser;
 var Generator = require('azk-parser').Generator;
 var fileUtils = require('azk-parser').fileUtils;
 var bb = require('bluebird');
 var glob = fileUtils.glob;
 var esprima = require('esprima-fb');
 
-var parser = new Parser({
-  raw: false,
-  tokens: false,
-  range: false,
-  comment: false,
-  loc: false
-});
+// ******************************************
+// This is the original recast parser
+//   but we can turn off some options
+//   like loc, comments, so the AST becomes
+//   hard to analise. We are using the same
+//   esprima version that recast;
+// ******************************************
+// var Parser = require('azk-parser').Parser;
+// var parser = new Parser({
+//   range: false,
+//   tolerant: false
+// });
 
 glob('./from-code/**/*.js', {}, function(err, files) {
   files.forEach(function(file) {
     fileUtils.read(file).then(function(content) {
-      // parse code to AST
-      // var ast = parser.parse(content).syntax;
+
+      // parse code to AST without any localization
       var ast = esprima.parse(content,
-        {raw: false, tokens: false, range: false, comment: false, loc: false });
+        { raw: false, tokens: false, range: false, comment: false, loc: false });
+      // var ast = parser.parse(content).syntax;
 
       // get destination path
       var parsed_path = path.parse(file);
